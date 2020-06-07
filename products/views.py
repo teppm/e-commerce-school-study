0d03c2af8dcbaf55b9c54904d771ab7bb3dc9a9e
unknown <madis.tepp@gmail.com>
2020-06-07 17:57:48 +0200

    created template folder, views, fixed urls.py for products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    '''a view to return all products, including sorting and search queries'''
+
+    products = Product.objects.all() #return all products from database
+ 
+    context = { #make products available in template
+        'products': products
+    }
+
+    return render(request, 'products/products.html', context) #context needed to add to database later on
