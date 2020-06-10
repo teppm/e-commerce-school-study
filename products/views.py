2a5c78ff34700790f0640ac6f40f44273b3d58a5
unknown <madis.tepp@gmail.com>
2020-06-10 17:08:10 +0200

    sorting , filtering

--- products/views.py
+++ products/views.py
@@ -11,8 +11,24 @@ def all_products(request):
     products = Product.objects.all() #return all products from database
     query = None #start with empty query so we dont get error on products page without a search term
     categories = None #starting with empty for same reason as above
+    sort = None #starting with empty for same reason as above
+    direction = None #starting with empty for same reason as above
 
     if request.GET: #if we receive a request.get from form
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if 'sortkey' == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'sortkey' == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET: #checking if category exists in request.get
             categories = request.GET['category'].split(',') # create variable with request.get content and split list from comma
             products = products.filter(category__name__in=categories) #And then use that list to filter the current query set of all products down to only products whose category name is in the list.
@@ -28,12 +44,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query) #creating or logic if query variable is found in either name or description return it
             products = products.filter(queries) #adding queries to the filter
 
-            
+    
+    current_sorting = f'{sort}_{direction}'
  
     context = { #make products available in template
         'products': products,
         'search_term': query, #search term is either the empty query or the request.get generated one
-        'current_categories': categories
+        'current_categories': categories,
+        'current_sorting' : current_sorting
     }
 
     return render(request, 'products/products.html', context) #context needed to add to database later on
