55807c77fca92f549c2a1afa504197ffc0064c2b
unknown <madis.tepp@gmail.com>
2020-07-20 14:03:29 +0200

    added order history and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -37,7 +37,7 @@ class OrderAdmin(admin.ModelAdmin):
     This way the order stays the same as it appears in the model.
     """
 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
+    fields = ('order_number', 'user_profile ''date', 'full_name', 'email', 'phone_number',
              'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2',
              'county', 'delivery_cost', 'order_total', 'grand_total', 'original_bag', 'stripe_pid')
 
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
+from checkout.models import  Order
 from .forms import UserProfileForm
 # Create your views here.
 
@@ -27,3 +28,18 @@ def profile(request):
 
     return render(request, template, context)
 
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past order confirmation for order number { order_number }.'
+        'A confirmation was sent to you to on the order date'
+    ))  
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
