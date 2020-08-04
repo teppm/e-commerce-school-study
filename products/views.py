e8d3f6053c56a0f92150ae8eb255dd730dd5b9cf
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-08-04 13:23:20 +0300

    add product form, fixed base / mobile top nav . html

--- products/views.py
+++ products/views.py
@@ -73,10 +73,21 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add product to store """
-    form = ProductForm()
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
-        'form': form
-    }
+            'form': form
+        }
 
     return render(request, template, context)
